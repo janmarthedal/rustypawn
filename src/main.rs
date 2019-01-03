1e33a6f0ddd85a4e9eb05656f88c0ff132539caa
Jan Marthedal Rasmussen <jan@janmr.com>
2019-01-03 16:42:21 +0100

    Add perft tests

--- src/main.rs
+++ src/main.rs
@@ -154,6 +154,7 @@ fn init_game(fen: &str) -> Result<Game, &str> {
                     'Q' => castling = castling | CASTLING_QUEEN_WHITE,
                     'k' => castling = castling | CASTLING_KING_BLACK,
                     'q' => castling = castling | CASTLING_QUEEN_BLACK,
+                    '-' => continue,
                     _ => return Result::Err("Illegal castling character")
                 }
             }
@@ -549,6 +550,76 @@ fn perft(fen: &str, depth: usize) -> usize {
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use super::perft;
+
+    #[test]
+    fn perft_initial_position() {
+        let fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0";
+        assert_eq!(perft(fen, 1), 20);
+        assert_eq!(perft(fen, 2), 400);
+        assert_eq!(perft(fen, 3), 8902);
+        assert_eq!(perft(fen, 4), 197281);
+    }
+
+    #[test]
+    fn perft_position_2() {
+        let fen = "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0";
+        assert_eq!(perft(fen, 1), 48);
+        assert_eq!(perft(fen, 2), 2039);
+        assert_eq!(perft(fen, 3), 97862);
+    }
+
+    #[test]
+    fn perft_position_3() {
+        let fen = "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0";
+        assert_eq!(perft(fen, 1), 14);
+        assert_eq!(perft(fen, 2), 191);
+        assert_eq!(perft(fen, 3), 2812);
+        assert_eq!(perft(fen, 4), 43238);
+        assert_eq!(perft(fen, 5), 674624);
+    }
+
+    #[test]
+    fn perft_position_4w() {
+        let fen = "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0";
+        assert_eq!(perft(fen, 1), 6);
+        assert_eq!(perft(fen, 2), 264);
+        assert_eq!(perft(fen, 3), 9467);
+        assert_eq!(perft(fen, 4), 422333);
+    }
+
+    #[test]
+    fn perft_position_4b() {
+        let fen = "r2q1rk1/pP1p2pp/Q4n2/bbp1p3/Np6/1B3NBn/pPPP1PPP/R3K2R b KQ - 0";
+        assert_eq!(perft(fen, 1), 6);
+        assert_eq!(perft(fen, 2), 264);
+        assert_eq!(perft(fen, 3), 9467);
+        assert_eq!(perft(fen, 4), 422333);
+    }
+
+    #[test]
+    fn perft_position_5() {
+        let fen = "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8";
+        assert_eq!(perft(fen, 1), 44);
+        assert_eq!(perft(fen, 2), 1486);
+        assert_eq!(perft(fen, 3), 62379);
+    }
+
+    #[test]
+    fn perft_position_6() {
+        let fen = "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10";
+        assert_eq!(perft(fen, 1), 46);
+        assert_eq!(perft(fen, 2), 2079);
+        assert_eq!(perft(fen, 3), 89890);
+    }
+
+}
+
 fn main() {
-    println!("Perft: {}", perft("rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8", 4));
+    println!("Initial position {}", perft("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0", 5));
+    println!("Kiwipete {}", perft("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq -", 4));
+    println!("Position 3 {}", perft("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - -", 5));
+    println!("Position 6 {}", perft("r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10", 4));
 }
