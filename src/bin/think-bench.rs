43bc3084b15b2dafb820da391b8da58aff22be1c
Jan Marthedal Rasmussen <jan@janmr.com>
2019-01-21 16:14:34 +0100

    Update think benchmark

--- src/bin/think-bench.rs
+++ src/bin/think-bench.rs
@@ -22,18 +22,19 @@ fn think_test(fen: &str, depth: usize) {
     let mut game = Game::from_fen(fen).unwrap();
     let mut comms = Comms {};
 
-    let start = Instant::now();
-
     let mv = match think(&mut game, 1 << 20, depth, &mut comms) {
         Some(m) => m,
         None => panic!("No legal move")
     };
 
     println!("bestmove {}", mv.to_algebraic());
-    println!("Time: {} ms", millis_since(&start));
 }
 
 fn main() {
+    let start = Instant::now();
+
     think_test("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0", 7);
     think_test("1rb2rk1/p4ppp/1p1qp1n1/3n2N1/2pP4/2P3P1/PPQ2PBP/R1B1R1K1 w - - 4 17", 6);
+
+    println!("Time: {} ms", millis_since(&start));
 }
